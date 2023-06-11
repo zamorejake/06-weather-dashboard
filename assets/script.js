4f6d42a914674dbc28af6dd2d6d8958f8d2ad23d
Jake <zamorejake@gmail.com>
2023-06-10 20:34:00 -0500

    Picked this back up after a while, setup api calls in console log and other css work

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,75 @@
-let lat = 0;
-let lon = 0;
-let weatherReport = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={3816cfa64f75dacc8898b9ab7f936b5e}";
+const key = "3816cfa64f75dacc8898b9ab7f936b5e";
 let submitButton = document.getElementById("submit");
-let cityValue = document.getElementById("search");
+var cityValue = document.getElementById("search");
 let cityHistory = document.getElementById("cityHistory");
+var currentCityList = JSON.parse(localStorage.getItem("cityInput"));
 
-document.addEventListener('DOMContentLoaded', function() {
-    let container = document.createElement('h4');
-    container.setAttribute('id', "cityList")
-    let currentCityList = JSON.parse(localStorage.getItem("cityInput"));
-   // let noCommaList = currentCityList.join('\n');
-    //container.textContent = (noCommaList);
-   // cityHistory.appendChild(container);
-    console.log("test")
-    currentCityList.forEach(function(city){
-        let container = document.createElement('h4');
-        container.textContent = city;
+document.addEventListener("DOMContentLoaded", function () {
+  let container = document.createElement("h4");
+  container.setAttribute("id", "cityList");
+  if (!currentCityList){
+    return
+  } else {
+    currentCityList.forEach(function (city) {
+        let container = document.createElement("span");
+        container.textContent = `${city} `;
         cityHistory.appendChild(container);
-    });
+      });
+  }
 });
 
-submitButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    let city = cityValue.value;
-    cityValue.value = '';
-    let container = document.createElement('h4');
-    let currentCityList = JSON.parse(localStorage.getItem("cityInput"));
-    if (currentCityList) {
-        currentCityList.push(city);
-        localStorage.setItem("cityInput", JSON.stringify(currentCityList));
-    } else {
-        let newCityList = [];
-        newCityList.push(city);
-        localStorage.setItem("cityInput", JSON.stringify(newCityList));
-    }
-    container.setAttribute('id', "cityList")
-    container.textContent = (city);
-    cityHistory.appendChild(container);
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let city = cityValue.value;
+  let container = document.createElement("span");
+  let currentCityList = JSON.parse(localStorage.getItem("cityInput"));
+  if (currentCityList) {
+    currentCityList.push(city);
+    localStorage.setItem("cityInput", JSON.stringify(currentCityList));
+  } else {
+    let newCityList = [];
+    newCityList.push(city);
+    localStorage.setItem("cityInput", JSON.stringify(newCityList));
+  }
+  container.setAttribute("id", "cityList");
+  container.textContent = city;
+  cityHistory.appendChild(container);
+  findCity();
 });
 
-cityHistory.addEventListener("click", function(e) {
-    e.stopPropagation();
-    let displayCity = e.target.textContent;
-    cityValue.value = displayCity;
+cityHistory.addEventListener("click", function (e) {
+  e.stopPropagation();
+  let displayCity = e.target.textContent;
+  cityValue.value = displayCity;
 });
+function findCity() {
+  let city = cityValue.value;
+  let cityFinder = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
+  console.log(cityFinder);
+  cityValue.value = "";
+
+  fetch(cityFinder)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      var name = data[0].name;
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      return {name, lat, lon};
+    })
+    .then((locData) => {
+        findWeather(locData.lat, locData.lon);
+      });
+}
+async function findWeather(lat,lon) {
+    var weatherReport = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+    fetch(weatherReport)
+    .then((info) => {
+      return info.json();
+    })
+    .then((week) => {
+        console.log(week);
+      })
+}
+
